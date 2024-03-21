a74c85f503ae8e8a615960dca806f6307b2fa734
Saba Giorkhelidze <sabagiorkhelidze@gmail.com>
2024-03-21 17:13:53 +0400

    addede basic user model with setting.py declaration for auth_user_model

--- demo/users/models.py
+++ demo/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+    description = RichTextField(null=True, blank=True)
+    is_author = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
