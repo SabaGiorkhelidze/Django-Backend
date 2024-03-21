23a72897aa25dd137a8e7d56f947b05e5b92d0d4
Saba Giorkhelidze <sabagiorkhelidze@gmail.com>
2024-03-21 17:20:07 +0400

    added config files in settings.py for ckeditori, and added  forms for admin to change users in users/forms.py

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -43,6 +43,13 @@ INSTALLED_APPS = [
     'users',
 ]
 
+# ckeditor config
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
